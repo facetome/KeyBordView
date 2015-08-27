cfc2c0c6449722aa3a0ea97763430e35cbd7d00a
facetome <cuit_liangsen@sina.com>
2015-08-27 22:43:02 +0800

    init project
    Signed-off-by: facetome <cuit_liangsen@sina.com>

---
+++ app/src/main/java/basic/keyboardviewa/view/CellView.java
@@ -0,0 +1,8 @@
+package basic.keyboardviewa.view;
+
+/**
+ * Created by basic on 2015/8/27.
+ * 该view的子view
+ */
+public class CellView {
+}
---
+++ app/src/main/java/basic/keyboardviewa/view/KeyBoardView.java
@@ -0,0 +1,28 @@
+package basic.keyboardviewa.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by basic on 2015/8/27.
+ */
+public class KeyBoardView extends View{
+    public KeyBoardView(Context context) {
+        super(context);
+    }
+
+    public KeyBoardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public KeyBoardView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+    }
+}
